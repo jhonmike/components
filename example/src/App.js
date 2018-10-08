c4bdb0cee6ff6dda19b68d125630b97792e54e65
Jhon Mike <developer@jhonmike.com.br>
2018-10-08 00:52:42 -0300

    fix docs

--- example/src/App.js
+++ example/src/App.js
@@ -1,26 +1,24 @@
 import React from 'react'
 
-import { Container, Button, Row, Col } from 'components'
+import { Container, Button, Row, Col, Text } from 'components'
 
 export default () => (
   <React.Fragment>
     <Container>
       <Row>
-        <Col xs={12} sm={4} md={3} lg={2}>
-          <Button>Button Default</Button>
-        </Col>
-        <Col xs={12} sm={4} md={3} lg={2}>
-          <Button theme="primary">Button Primary</Button>
-        </Col>
-        <Col xs={12} sm={4} md={3} lg={2}>
-          <Button theme="secondary">Button Secondary</Button>
+        <Col xs>
+          <Text tagName='h1'>Buttons</Text>
         </Col>
       </Row>
-    </Container>
-    <Container fluid>
       <Row>
-        <Col xsOffset={6} xs={6}>
+        <Col xs={12} sm={4} md={3} lg={2}>
           <Button>Button Default</Button>
+        </Col>
+        <Col xs={12} sm={4} md={3} lg={2}>
+          <Button theme='primary'>Button Primary</Button>
+        </Col>
+        <Col xs={12} sm={4} md={3} lg={2}>
+          <Button theme='secondary'>Button Secondary</Button>
         </Col>
       </Row>
     </Container>
