ebda344f8befbb73bbdc0cb12e741c10155c8e1d
Jhon Mike <developer@jhonmike.com.br>
2019-01-31 13:44:47 -0200

    tests storybook

--- src/button.stories.js
+++ src/button.stories.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
 import Button from './Button'
 
-storiesOf('Button', module).add('with text', () => (
-  <Button>Hello Button</Button>
-))
+storiesOf('Button', module).add(
+  'with text',
+  () => <Button onClick={action('clicked')}>Hello Button</Button>,
+  { info: { inline: true, header: true } }
+)
